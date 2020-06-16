a0248b67bc9e52113618a99df61726e42685086f
Fefefo <tardiolifederico@gmail.com>
2020-06-16 02:27:46 +0200

    Is odd?

---
+++ go.go
@@ -0,0 +1,19 @@
+package isodd
+
+import (
+	"fmt"
+	"strconv"
+)
+
+// IsOdd returns true if the number is odd WITHOUT USING THE %
+func IsOdd(num float64) bool {
+	num = num / 2
+
+	n1, _ := strconv.ParseFloat(fmt.Sprintf("%f", num), 64)
+	n2, _ := strconv.ParseFloat(fmt.Sprintf("%.0f", num), 64)
+
+	if n1 != n2 {
+		return true
+	}
+	return false
+}
